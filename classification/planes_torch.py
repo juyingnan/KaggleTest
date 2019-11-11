72a96861c0d1dd3dc56d82e655d73802615ef8e1
Yingnan Ju <bunny_gg@hotmail.com>
2019-11-11 18:00:23 -0500

    change structure to resnet

--- classification/planes_torch.py
+++ classification/planes_torch.py
@@ -42,10 +42,10 @@ def set_parameter_requires_grad(model, feature_extracting):
 def initialize_model(num_classes, feature_extract, use_pretrained=True):
     # Initialize these variables which will be set in this if statement. Each of these
     #   variables is model specific.
-    model_ft = models.alexnet(pretrained=use_pretrained)
+    model_ft = models.resnet18(pretrained=use_pretrained)
     set_parameter_requires_grad(model_ft, feature_extract)
-    num_ftrs = model_ft.classifier[6].in_features
-    model_ft.classifier[6] = nn.Linear(num_ftrs, num_classes)
+    num_ftrs = model_ft.fc.in_features
+    model_ft.fc = nn.Linear(num_ftrs, num_classes)
     input_size = 224
 
     return model_ft, input_size
